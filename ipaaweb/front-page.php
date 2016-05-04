743d3e335d2ae74d8416eeba7846128bb41455ff
avih8r <sparco.209@gmail.com>
2016-05-04 14:40:53 +1000

    Doing homepage widget areas

--- ipaaweb/front-page.php
+++ ipaaweb/front-page.php
@@ -17,6 +17,7 @@ GREETINGS FROM front-page.php
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 		<div id="events-section">
+		<?php dynamic_sidebar( 'home_right_1' ); ?>
 		events section goes here
 		</div>
 
