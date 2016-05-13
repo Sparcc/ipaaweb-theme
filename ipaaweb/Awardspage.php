5d715d53726e68e9c1be2cfed8743a14bf5d037b
jchen33 <chenjian.hb@gmail.com>
2016-05-13 12:38:33 +1000

    Update and rename Awardspage.html to Awardspage.php
    delete <html> <body> <head> tag

--- ipaaweb/Awardspage.php
+++ ipaaweb/Awardspage.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>无标题文档</title>
-<link href="Awards.css" rel="stylesheet" type="text/css" />
-</head>
-
-<body class="Awards">
 <div id="Awardsimage"><img src="Awards.png" width="156" height="46" /></div>
 <hr />
 <div class="awardsidebar" id="sidebar">
@@ -43,5 +34,3 @@
     </table>
   </div>
 </div>
-</body>
-</html>
