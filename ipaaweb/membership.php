908220f66e55b35977f5f4bd1cd6418fa68fb4d2
vijayThapa <manzil.close2u@gmail.com>
2016-05-16 11:40:00 +1000

    membership page
    css styling on bottom of style.css

---
+++ ipaaweb/membership.php
@@ -0,0 +1,107 @@
+
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+
+<div class="outer">
+
+<div class = "leftcol">
+
+<h1 class = "vij-h1">Benefits of membership</h1>
+
+<h3>There are a wide range of personal, professional and corporate benefits from membership of IPAA, including the following:<h3>
+
+<p>
+<ol>
+
+<li> All private members, including nominated corporate member representatives, receive the IPAA quarterly publication: the Australian Journal of Public Administration, a peer reviewed journal committed to the study and practice of public administration, public management and policy making.</li>
+<li> Lower admission fees to forums, seminars and special events.</li>
+<li> Opportunities to network with people at all levels, from other areas of government and promote yourself and your organisation. </li>
+<li> Provision of information and insights in relation to what is happening elsewhere at the Local, State and Commonwealth Government levels.</li>
+<li> Learning opportunities through the professional development program.</li>
+<li> Opportunities to share ideas contribute to the development of public sector administration in the state and build public sector management as a profession.</li>
+<li> Discover the wider public service and understand the role of other areas of the public service. </li>
+<li> Develop leadership qualities.</li>
+<li> A program of awards that recognise excellence of public sector teams, individuals and students.</li>
+<li> Share ideas and promote the value of the public service and build the profession. </li>
+
+</ol>
+</p>
+
+<br>
+
+</div>
+
+
+
+
+<div class = "middlecol">
+<h1 class = "vij-h1"> Types </h1>
+<h2 class = "vij-h2"> Individual Membership</h2>
+<img class="alignnone size-medium wp-image-80" src="individual1.jpg" alt="individual1" width="225" height="225" />
+
+<p> The fee for an individual member is $121.00 inc GST for the 2015/16 membership year.
+  A special rate of $57.20 inc GST is provided for full time students, retired members and current students in the PSM Program. 
+  As an individual member you will receive all the benefits listed below.</p>
+<h2 class = "vij-h2"> Corporate Membership</h2>
+<img class="size-medium wp-image-78" src="corp-member1.jpg" alt="pic crop member" width="300" height="225" />
+
+<p> In addition to providing access to a wide range of benefits for staff, by becoming a Corporate Member of the Institute of Public Administration (IPAA)
+ your organisation can support the advancement of public sector administration in Tasmania.</p>
+
+<p>There are three different corporate membership levels based on the size of your organisation or business unit. 
+ Corporate membership entitles a prescribed number of nominated staff from your organisation or business unit 
+ to all the benefits and rights of private membership, plus lower admission fees to events held by the Institute for all your staff. </p>
+
+ 
+
+ </div>
+
+
+
+
+
+<div class = "rightcol">
+<h1 class = "vij-h1"> Corporate Membership Are : </h1>
+<p>
+<ul>
+<li>Australian Bureau of Statistics </li>
+<li>Department of Education </li>
+<li>Department of Employment (Australian Government) </li>
+<li>Department of Health and Human Services </li>
+<li>Department of Human Services (Australian Government) </li>
+<li>Department of Justice </li>
+<li>Department of Police and Emergency Management </li>
+<li>Department of Premier and Cabinet </li>
+<li>Department of Primary Industries, Parks, Water and Environment </li>
+<li>Department of Social Services (Australian Government) </li>
+<li>Department of State Growth </li>
+<li>Department of Treasury and Finance </li>
+<li>Hobart City Council </li>
+<li>Integrity Commission of Tasmania </li>
+<li>Motor Accidents Insurance Board </li>
+<li>Office of the Ombudsman and Health Complaints Commissioner </li>
+<li>Tasmanian Audit Office </li>
+
+
+</ul>
+</p>
+
+<br> <br><br><br><br><br>
+
+<p>
+<table>
+<thead><tr><th></th><th>Subscription Fee</th><th>No. of Corp Tepresentative</th></tr></thead>
+<tbody><tr><td>Less Than 100</td><td>$363 inc GST</td><td> 3 </td></tr>
+<tr><td>Between 100 to 500</td><td>$726 inc GST</td><td> 6 </td></tr>
+<tr><td>Over 500 employees</td><td>$1210 inc GST</td><td> 10</td></tr>
+</tbody>
+</table>
+</p>
+
+<br><br><br><br><br><br><br>
+
+</div>
+</div>
+
+
