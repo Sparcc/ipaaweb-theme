1a34706588c00432b95917b937f6f1b0c960ddf8
avih8r <sparco.209@gmail.com>
2016-05-21 15:09:29 +1000

    made footer

--- ipaaweb/footer.php
+++ ipaaweb/footer.php
@@ -15,7 +15,8 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<?php //<span class="sep"> SEPARATOR </span>?>
-			<p><input type="button" value="Terms and conditions"></input>
+			<div align="left">
+            <font size="2" color="#eeede5" face="Verdana, Arial, Helvetica, sans-serif">&nbsp;&nbsp;IPAA Tasmania © 2010 Please email <a href="mailto:ttc@dpac.tas.gov.au"><font color = "white">ttc@dpac.tas.gov.au</font></a> with questions and comments about this website. </font></div>
 			<?php //printf( esc_html__( 'Theme: %1$s by %2$s.', 'ipaaweb' ), 'ipaaweb', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
