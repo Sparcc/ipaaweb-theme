548074c72d6ddcc2ba1d1b32fa7812efb395ff8c
jchen33 <chenjian.hb@gmail.com>
2016-05-21 15:09:51 +1000

    Update linkpage.php

--- ipaaweb/linkpage.php
+++ ipaaweb/linkpage.php
@@ -1,32 +1,60 @@
 <div>
-  <div align="justify"><img src="http://alacritas.cis.utas.edu.au/~ipaaweb/wp-content/uploads/2016/04/links.png" width="127" height="55" align="top" />  </div>
+  <div><h1>Links</hi></div>
 </div>
 <hr />
 <div id="links">
   <div align="center">
-    <table width="100%" border="0">
+    <table width="100%" border="0" cellspacing="0" cellpadding="0">
       <tr>
-        <td width="37%"><p>&nbsp;</p>
-        <p><img src="http://alacritas.cis.utas.edu.au/~ipaaweb/wp-content/uploads/2016/04/National-Division.png"  class="linkimagepostion" /></p>
-        <p>&nbsp;</p></td>
-        <td width="63%" class="linkpageWords"><a href="http://www.ipaa.org.au" target="_blank">Institute of Public Administration- National Office</a></td>
-      </tr>
-      <tr>
-        <td><p>&nbsp;</p>
-          <p><img src="http://alacritas.cis.utas.edu.au/~ipaaweb/wp-content/uploads/2016/04/Training.png" class="linkimagepostion" /></p>
-        <p>&nbsp;</p></td>
-        <td class="linkpageWords"><a href="http://www.ttc.tas.gov.au" target="_blank">The Training Consortium</a></td>
+        <th scope="col"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <th rowspan="12" scope="col"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td align="center"><a href="http://www.ipaa.org.au/" target="_blank"><img src="http://alacritas.cis.utas.edu.au/~ipaaweb/wp-content/uploads/2016/04/National-Division.png"  class="linkimagepostion" /></a></td>
+              </tr>
+              <tr>
+                <td align="center"><a href="http://www.ttc.tas.gov.au/" target="_blank"><img src="http://alacritas.cis.utas.edu.au/~ipaaweb/wp-content/uploads/2016/04/Training.png"  class="linkimagepostion" /></a></td>
+              </tr>
+            </table></th>
+            <th scope="col">&nbsp;</th>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td align="left"><h2>Other IPAA Divisions</h2></td>
+          </tr>
+          <tr>
+            <td align="left"><li><a href="http://www.act.ipaa.org.au/" target="_blank">IPAA Australian Capital Territory</a></li></td>
+          </tr>
+          <tr>
+            <td align="left"><li><a href="http://nt.ipaa.org.au/" target="_blank">IPAA Northern Territory</a></li></td>
+          </tr>
+          <tr>
+            <td align="left"><li><a href="http://www.sa.ipaa.org.au/" target="_blank">IPAA South Australia</a></li></td>
+          </tr>
+          <tr>
+            <td align="left"><li><a href="http://www.vic.ipaa.org.au/" target="_blank">IPAA Victoria</a></li></td>
+          </tr>
+          <tr>
+            <td align="left"><li><a href="http://www.wa.ipaa.org.au/" target="_blank">IPAA Western Australia</a></li></td>
+          </tr>
+          <tr>
+            <td align="left"><li><a href="http://www.nsw.ipaa.org.au/" target="_blank">IPAA New South Wales</a></li></td>
+          </tr>
+          <tr>
+            <td align="left"><li><a href="http://www.ipaa.org.au/qld/" target="_blank">IPAA Queensland</a></li></td>
+          </tr>
+          <tr>
+            <td><p><br />
+            </p></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+        </table></th>
       </tr>
     </table>
   </div>
 </div>
-<div id="OtherDisvision"><img src="http://alacritas.cis.utas.edu.au/~ipaaweb/wp-content/uploads/2016/04/other-division.png"  />
-  <hr />
-  <p><br />
-  <span class="OtherDisvisionsLeft">Visit the <a href="http://nt.ipaa.org.au">IPAA NT</a> Website</span> <span class="OtherDisvisionsRight"> Visit the <a href="http://www.act.ipaa.org.au">IPAA ACT</a> Website</span></p>
-  <p><span class="OtherDisvisionsLeft">Visit the <a href="http://www.sa.ipaa.org.au">IPAA SA</a> Website</span> <span class="OtherDisvisionsRight">Visit the <a href="http://www.ipaa.org.au/qld/">IPAA QLD</a> Website</span></p>
-  <p><span class="OtherDisvisionsLeft">Visit the <a href="http://www.wa.ipaa.org.au">IPAA WA</a> Website</span> <span class="OtherDisvisionsRight">Visit the <a href="https://www.vic.ipaa.org.au">IPAA VIC</a> Website</span></p>
-  <p><span class="OtherDisvisionsLeft">Visit the <a href="http://www.nsw.ipaa.org.au">IPAA NSW</a> Website</span></p>
-</div>
 <p>&nbsp;</p>
-
