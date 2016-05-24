4234d0b60c002535f6e371534c73e8a0f1855547
jchen33 <chenjian.hb@gmail.com>
2016-05-24 21:36:18 +1000

    Add files via upload

---
+++ ipaaweb/photogallery.php
@@ -0,0 +1,54 @@
+<style>
+div.img {
+    margin: 60px; 
+    float: left;
+    width: 200px;
+}
+
+div.img:hover {
+    border: 1px solid #777;
+}
+
+div.img img {
+	border: 1px solid #ddd;
+    border-radius: 4px;
+    padding: 5px;
+	border-radius: 8px;
+    width: 100%;
+    height: auto;
+}
+
+div.desc {
+    padding: 15px;
+    text-align: center;
+}
+</style>
+<div><h1>Photo Gallery</h1></div>
+
+<div class="img">
+  <a target="_blank" href="http://alacritas.cis.utas.edu.au/~ipaaweb/wp-content/uploads/2016/04/wineglass-bay.jpg">
+    <img src="http://alacritas.cis.utas.edu.au/~ipaaweb/wp-content/uploads/2016/04/wineglass-bay.jpg" alt="Trolltunga Norway" width="600" height="400">
+  </a>
+  <div class="desc">Wineglass Bay</div>
+</div>
+
+<div class="img">
+  <a target="_blank" href="http://alacritas.cis.utas.edu.au/~ipaaweb/wp-content/uploads/2016/04/tasman_bridge.jpg">
+    <img src="http://alacritas.cis.utas.edu.au/~ipaaweb/wp-content/uploads/2016/04/tasman_bridge.jpg" alt="Forest" width="600" height="400">
+  </a>
+  <div class="desc">Tasman Bridge</div>
+</div>
+
+<div class="img">
+  <a target="_blank" href="http://alacritas.cis.utas.edu.au/~ipaaweb/wp-content/uploads/2016/04/Mount_Wellington.jpg">
+    <img src="http://alacritas.cis.utas.edu.au/~ipaaweb/wp-content/uploads/2016/04/Mount_Wellington.jpg" alt="Northern Lights" width="600" height="400">
+  </a>
+  <div class="desc">Mount Wellington</div>
+</div>
+
+<div class="img">
+  <a target="_blank" href="http://alacritas.cis.utas.edu.au/~ipaaweb/wp-content/uploads/2016/04/Port Arthur.jpg">
+    <img src="http://alacritas.cis.utas.edu.au/~ipaaweb/wp-content/uploads/2016/04/Port Arthur.jpg" alt="Mountains" width="600" height="400">
+  </a>
+  <div class="desc">Port Arthur</div>
+</div>
