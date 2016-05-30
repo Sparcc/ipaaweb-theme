11a09286e4ebbd5de03260d4bb449856f826999a
jchen33 <chenjian.hb@gmail.com>
2016-05-30 15:03:39 +1000

    Update aboutus.php
    do not change

--- ipaaweb/aboutus.php
+++ ipaaweb/aboutus.php
@@ -1,4 +1,31 @@
+<style>
+.AboutUsBody {
+	height: 100%;
+	position: absolute;
+	overflow: auto;
+	width: 80%;
+	left: 10%;
+	right: 10%;
+}
+.aboutusbigtitle {
+	font-size: 26px;
+	color: #000;
+}
+.aboutuscontent {
+	font-size: 18px;
+	color: #000;
+}
+
+.ContentPosition {
+	margin-left: 5%;
+	margin-right: 5%;
+	font-size: 18px;
+	position: relative;
+	line-height: 36px;
+}
+</style>
 <div>
+<link href="http://alacritas.cis.utas.edu.au/~ipaaweb/wp-content/themes/ipaaweb/style.css" rel="stylesheet" type="text/css" />
 <div align="justify">
   <div><h1>About us</h1></div>
 </div>
@@ -6,10 +33,10 @@
 <hr />
 <div id="content">
   <h2 class="aboutusbigtitle">Who are IPAA's Members?</h2>
-  <div id="IPAAmemberIntroduce"><span class="ContentPosition">Private and corporate membership is extensive and is drawn from Commonwealth and State<br />
-    Government Agencies, Local Government, Government Business Enterprises, Statutory Authorities, <br />
-Public Bodies and the University. IPAA also has several private sector members, as our<br />
-organisation represents a key resource for gaining insight into current public sector management issues.</span>  </div>
+  <div id="IPAAmemberIntroduce"><p class="ContentPosition">Private and corporate membership is extensive and is drawn from Commonwealth and State
+    Government Agencies, Local Government, Government Business Enterprises, Statutory Authorities,
+Public Bodies and the University. IPAA also has several private sector members, as our
+organisation represents a key resource for gaining insight into current public sector management issues.</p> </div>
   <h2 class="aboutusbigtitle">What Does the Institute Provide?</h2>
   <ul>
     <li>
@@ -31,10 +58,30 @@ organisation represents a key resource for gaining insight into current public s
       <p class="aboutuscontent">the Australian Journal of Public Administration, the IPAA's authoritative national quarterly publication</p>
     </li>
     <li>
-      <p class="aboutuscontent">a three-day National Conference hosted by each Division in turn.</p>
+      <p class="aboutuscontent">a three-day National Conference hosted by each Division in turn</p>
     </li>
   </ul>
-  <p>&nbsp;</p>
-  <p>&nbsp;</p>
+</div>
+  <div class="aboutusbigtitle" id="corevalue">
+    <h2 class="aboutusbigtitle">Core Membership Value Proposition  </h2>
+  </div>
+  <div id="CoreValueContent">
+    <p class="ContentPosition"><strong><em><strong>Leadership</strong></em></strong></p>
+    <p class="ContentPosition">the IPAA provides <em><strong><em>leadership </em></strong></em>by promoting its core organisational values and providing opportunities to develop leadership skills. The IPAA will actively encourage the highest standards of leadership.</p>
+    <p class="ContentPosition"><strong><em>Professional Development</em></strong></p>
+    <p class="ContentPosition">the IPAA provides <strong><em><strong>professional development </strong></em></strong>by making access available to relevant opportunities to develop skills and knowledge. The IPAA will actively encourage personal development and career advancement.</p>
+    <p class="ContentPosition"><strong><em><strong>Networking</strong></em></strong></p>
+    <p class="ContentPosition"><span class="aboutuscontent">the IPAA provides <em><strong><em>networking </em></strong></em>opportunities that will assist in developing professional and personal relationships across the public sector and academic institutions. The IPAA will actively encourage greater business and social collaboration.</span></p>
+  </div>
+  <div id="Core Organisation Values">
+    <h2><span class="aboutusbigtitle">Core Organisation Values</span></h2>
+  </div>
+<div id="CoreOrganisationValues">
+  <p class="ContentPosition"><strong><em><strong>Professionalism</strong></em></strong></p>
+  <p class="ContentPosition">the IPAA is <em><strong><em>professional </em></strong></em>- by striving for excellence through taking care and pride in our professional lives.</p>
+  <p class="ContentPosition"><strong><em><strong>Integrity</strong></em></strong></p>
+  <p class="ContentPosition">the IPAA acts with <strong><em><strong>integrity </strong></em></strong>- by adhering to well established public sector and community moral and ethical principles.</p>
+  <br />
+</div>
 </div>
 <p>&nbsp;</p>
